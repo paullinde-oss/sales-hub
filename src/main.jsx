94f59739618809bb0a3c86d03cb33bd2828efac3
paullinde-oss <paul.lindemulder@gmail.com>
2026-05-27 16:08:22 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SalesHub from './SalesHub'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SalesHub />
+  </React.StrictMode>
+)

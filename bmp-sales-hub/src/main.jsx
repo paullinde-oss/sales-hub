42d8b525cc35b3e7e0b178ec5860b0ff68be3c69
paullinde-oss <paul.lindemulder@gmail.com>
2026-05-27 16:05:44 -0700

    Delete bmp-sales-hub directory

--- bmp-sales-hub/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SalesHub from './SalesHub'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SalesHub />
-  </React.StrictMode>
-)
